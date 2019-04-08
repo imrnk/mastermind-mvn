2d4fab286541936cd06d14affa74a05aa6f4d6c4
imrnk <sujoy.imran@gmail.com>
2019-04-08 23:25:38 +0530

    Corrected number generator with set

--- src/main/java/NumberCombinators.java
+++ src/main/java/NumberCombinators.java
@@ -1,50 +1,53 @@
 import org.apache.commons.math3.util.CombinatoricsUtils;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class NumberCombinators {
 
     public static void main(String[] args) {
-      // generate(3,2);
-        generateCombinations(4);
+        Set<int[]> allCombi = generateCombinations(4);
+        System.out.println(allCombi.size());
     }
 
 
-    /*public static void generate(int n, int r) {
-        Iterator<int[]> iterator = CombinatoricsUtils.combinationsIterator(n, r);
-        while (iterator.hasNext()) {
-            final int[] combination = iterator.next();
-            System.out.println(Arrays.toString(combination));
+    public static void print(Set<int[]> s) {
+        for(int[] a : s) {
+            for(int j : a) {
+                System.out.print(j + " ");
+            }
+            System.out.println(" ");
         }
-    }*/
+    }
 
     public static Set<int[]> generateCombinations(int length) {
         Set<int[]> allCombi = new HashSet<>();
         int[] n = new int[length];
-        makeAtt(0, 10, length, n);
-        allCombi.add(n);
+        makeAtt(0, 10, length, n, allCombi);
+        //allCombi.add(n);
         return allCombi;
     }
 
-    private static void makeAtt(int min, int max, int depth, int[] vals) {
+    private static void makeAtt(int min, int max, int depth, int[] vals, Set<int[]> allCombi) {
         //Guard against bad depth
-        if (depth <= 0) return;
-       /* if (depth == 0) {
-            System.out.print("{");
-            for (int d : vals) {
-                System.out.print(d + " ");
+        if (depth < 0) return;
+        if (depth == 0) {
+            int half = vals.length /2;
+            int[] finalArr = new int[half];
+
+            for(int j = half -1 , k =0; j < vals.length - 1; j++, k++) {
+                finalArr[k] = vals[j+1];
             }
-            System.out.print("}\n\n");
-        }*/ else {
+
+            allCombi.add(finalArr);
+
+        } else {
             for (int i = min; i < max; i = i + 1) {
                 int[] newVals = new int[vals.length + 1];
                 for (int z = 0; z < vals.length; z++)
                     newVals[z] = vals[z];
                 newVals[vals.length] = i;
-                makeAtt(min, max, depth - 1, newVals);
+
+                makeAtt(min, max, depth - 1, newVals, allCombi);
             }
         }
     }
@@ -56,7 +59,6 @@ public class NumberCombinators {
             dig[i] = 9;
         }
 
-
         int maxnum = 0;
         for (int i = 1; i < dig.length; i++) {
             maxnum += Math.pow(10, dig.length - i) * dig[i];
@@ -65,26 +67,17 @@ public class NumberCombinators {
     }
 
     public void garbage() {
+
         int n = 46132;
-
         int nlength = (int) Math.floor(Math.log10(n));
-
-
         int t = (int) n / (10 ^ nlength);
         int h = (int) (n - t ^ nlength) / 100;
         int d = (int) (n - t * 1000 - h * 100) / 10;
         int r = (int) (n - t * 1000 - h * 100 - d * 10);
-
-
         System.out.println(t);
         System.out.println(h);
         System.out.println(d);
         System.out.println(r);
-
-
-        //double place = (int)Math.pow(10, tenthPower);
-
-        // System.out.println(tenthPower);
     }
 
 
